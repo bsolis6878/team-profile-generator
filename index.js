7317909202ac035d785367c382b8295aef46b619
bsolis6878 <bsolis6878@gmail.com>
2022-09-16 16:17:08 -0500

    questions prompt for employee and intern, will probably have to restructure

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 
-const initialQuestion = () => {
+const initialQuestions = () => {
     return inquirer.prompt([
         {
             type: 'input',
@@ -53,11 +53,137 @@ const initialQuestion = () => {
                     return false;
                 }
             }
+        },
+        {
+            type: 'list',
+            name: 'continue',
+            message: "Would you like to add an engineer or intern to your team, or finish and generate the document?",
+            choices: ['Engineer', 'Intern', 'Finish']
         }
     ])
 }
 
-initialQuestion()
+const engineerQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'engineerName',
+            message: "What is the engineer's name?",
+            validate: engineerNameInput => {
+                if (engineerNameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the engineer's name.")
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'engineerId',
+            message: "What is the engineer's employee ID?",
+            validate: engineerIdInput => {
+                if (engineerIdInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the engineer's employee ID.")
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'engineerEmail',
+            message: "What is the engineer's email address?",
+            validate: engineerEmailInput => {
+                if (engineerEmailInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the engineer's email address.")
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'engineerGithub',
+            message: "What is the engineer's Github username?",
+            validate: engineerGithubInput => {
+                if (engineerGithubInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the engineer's Github username.")
+                    return false;
+                }
+            }
+        }
+    ])
+}
+
+const internQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'internName',
+            message: "What is the intern's name?",
+            validate: internNameInput => {
+                if (internNameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the intern's name.")
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'internId',
+            message: "What is the intern's employee ID?",
+            validate: internIdInput => {
+                if (internIdInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the intern's employee ID.")
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'internEmail',
+            message: "What is the intern's email address?",
+            validate: internEmailInput => {
+                if (internEmailInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the intern's email address.")
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'internSchool',
+            message: "What is the name of the intern's school?",
+            validate: internSchoolInput => {
+                if (internSchoolInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the name of the intern's school.")
+                    return false;
+                }
+            }
+        }
+    ])
+}
+
+initialQuestions()
     .then(data => {
-        console.log(data);
-    })
+        if (data.continue === 'Engineer') {
+            engineerQuestions()
+        } else if (data.continue === 'Intern') {
+            internQuestions();
+        } else if (data.continue === 'Finish') {
+            // createHtml();
+        }
+    });
